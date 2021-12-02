1b43d129a611e7143555f078c14878986886315c
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2021-12-02 11:42:54 +0530

    Add search functionality

--- src/main/java/com/everest/airline/services/SearchService.java
+++ src/main/java/com/everest/airline/services/SearchService.java
@@ -1,21 +1,19 @@
-package com.everest.airline.srvices;
+package com.everest.airline.services;
 
 import com.everest.airline.DTO.Flight;
 import com.everest.airline.Data;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class SearchService {
-    public List<Flight> getFlights(String from,String to){
-        List<Flight> result = null;
-        for (Flight flight :
-                Data.flights) {
-            if(flight.getSource()==from && flight.getDestination()==to)
-            result.add(flight);
-        }
-        System.out.println(result);
+    public List<Flight> getFlights(String from, String to) {
+        List<Flight> result = new ArrayList<>();
+        Data.flights.stream()
+                .filter(flight -> flight.getDestination().equals(to) && flight.getSource().equals(from))
+                .forEach(flight -> result.add(flight));
         return result;
     }
 }
