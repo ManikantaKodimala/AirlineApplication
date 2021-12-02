2d7ff574ec8a3ec6058f302ee41094ad25c62977
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2021-12-02 11:27:13 +0530

    madified as per story1

--- src/main/java/com/everest/airline/controllers/SearchController.java
+++ src/main/java/com/everest/airline/controllers/SearchController.java
@@ -1,12 +1,19 @@
-package com.everest.airline;
+package com.everest.airline.controllers;
 
+import com.everest.airline.DTO.Flight;
+import com.everest.airline.Data;
+import com.everest.airline.services.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class SearchController {
-
+    @Autowired
+    private SearchService searchService;
     @RequestMapping(value = "/")
     public String home() {
         return "home";
@@ -14,8 +21,8 @@ public class SearchController {
 
     @RequestMapping(value = "/search")
     public String search(String from, String to, Model model) {
-        searchData=getFlights()
-        model.addAttribute("flights", Data.flights);
+        List<Flight> searchData = searchService.getFlights(from, to);
+        model.addAttribute("flights", searchData);
         return "search";
     }
 }
