c67be4ad9b89f27dd66dcc4fb562264647ceeb65
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2021-12-17 13:24:48 +0530

    Refacotred code

--- src/main/java/com/everest/airline/DTO/BookingDTO.java
+++ src/main/java/com/everest/airline/DTO/BookingDTO.java
@@ -28,40 +28,15 @@ public class BookingDTO {
 
     private void updateFile(Flight flight) throws IOException {
         String path = "/Users/kodimalamanikanta/IdeaProjects/airlines/src/main/java/com/everest/airline/database/flightsdata/"+flight.getNumber()+".txt";
-        File folder = new File(path);
         Path filePath = Path.of(path);
-//        List<File> listOfFilesInDirectory = List.of(folder.listFiles());
-//
-//        listOfFilesInDirectory = listOfFilesInDirectory.stream().filter(file -> {
-//            String fileName = file.getName();
-//            fileName = fileName.substring(0, fileName.length() - 4);
-//            return fileName.equals(String.valueOf(flight.getNumber()));
-//        }).collect(Collectors.toList());
-//         path +="/" + listOfFilesInDirectory.get(0).getName();
-//        Files.write(filePath, List.of(flight.getFlightDetails()));
         Files.write(filePath, List.of(flightDTO.getFlightDetails(flight)));
     }
 
     public Flight getFlight(int flightNum) throws IOException, ParseException {
         String path = "/Users/kodimalamanikanta/IdeaProjects/airlines/src/main/java/com/everest/airline/database/flightsdata/"+flightNum+".txt";
-        File folder = new File(path);
         Path filePath = Path.of(path);
         List<String> lines = Files.readAllLines(filePath);;
         Flight flightInstance=flightDTO.getFlightInstance(lines.get(0));
-//        List<File> listOfFilesInDirectory = List.of(folder.listFiles());
-//        List<Flight> flightList = listOfFilesInDirectory.stream().filter(file -> file.getName().equalsIgnoreCase(flightNum + ".txt"))
-//                .map(file -> {
-//                    Flight flightInstance = null;
-//                    try {
-//                        List<String> lines = Files.readAllLines(Path.of(file.getPath()));
-//                        flightInstance = flightDTO.getFlightInstance(lines.get(0));
-//
-//                    } catch (IOException | ParseException e) {
-//                        e.printStackTrace();
-//                    }
-//                    return flightInstance;
-//                }).collect(Collectors.toList());
-//        return flightList.get(0);
         return flightInstance;
     }
 }
