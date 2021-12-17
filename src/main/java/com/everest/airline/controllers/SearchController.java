1a85151dfd6238bdd4054a736318cd2bf658d034
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2021-12-17 14:09:08 +0530

    Add price strtagey by available seats

--- src/main/java/com/everest/airline/controllers/SearchController.java
+++ src/main/java/com/everest/airline/controllers/SearchController.java
@@ -2,10 +2,12 @@ package com.everest.airline.controllers;
 
 import com.everest.airline.DTO.BookingDTO;
 import com.everest.airline.model.Flight;
+import com.everest.airline.services.PriceStrategyService;
 import com.everest.airline.services.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,10 @@ import java.util.List;
 public class SearchController {
     @Autowired
     private SearchService searchService;
+
+    @Autowired
+    private PriceStrategyService priceStrategyService;
+
     @Autowired
     private BookingDTO bookingDTO;
 
@@ -27,10 +33,10 @@ public class SearchController {
     @RequestMapping(value = "/search")
     public String search(String from,String to,String departureDate ,String classType,String numberOfPassengers,Model model) throws Exception {
         List<Flight> searchData = searchService.getFlights(from, to,departureDate,classType,Integer.parseInt(numberOfPassengers));
-        model.addAttribute("flights", searchData);
-        System.out.println(classType);
         model.addAttribute("selectedClassType",classType);
         model.addAttribute("numberOfPassengers",numberOfPassengers);
+        searchData.stream().forEach(flight -> priceStrategyService.updateTotalFaire(flight,Integer.parseInt(numberOfPassengers),classType));
+        model.addAttribute("flights", searchData);
         return "search";
     }
 
